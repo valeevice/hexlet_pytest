0b97a9b4078cc626f1feff74c84463675bb0ab5d
Алесей Валеев <valeevice@mail.ru>
2022-12-13 22:35:54 +0300

    make pytests

---
+++ tests/test_example.py
@@ -0,0 +1,10 @@
+# from hexlet_pytest import example
+from example import reverse
+
+
+def test_reverse():
+    assert reverse('Hexlet') == 'telxeH'
+
+
+def test_reverse_for_empty_string():
+    assert reverse('') == ''
